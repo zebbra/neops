369f9669293dd8215bc0c9f0b582b92a0de940c7
Leandro <leandro@lerena.ch>
2026-02-11 11:34:19 +0100

    Update and fix documentation assets (#11)
    * update script
    
    * update submodule
    
    * update doc asset location
    
    * update submodules
    
    * update submodule name
    
    * update submodule name
    
    * update references
    
    * update references
    
    * remove unused symlink
    
    * Update doc

--- .make_scripts/mkdocs-documentation/setup_documentation.sh
+++ .make_scripts/mkdocs-documentation/setup_documentation.sh
@@ -14,17 +14,18 @@ fi
 
 # Here, we know that all dependencies are installed and we are in the correct PWD
 
-mkdir make_scripts || true
-mkdir make_scripts/mkdocs-documentation || true
+mkdir .make_scripts || true
+mkdir .make_scripts/mkdocs-documentation || true
 tmp_folder=$(mktemp -d)
 gh release --repo "${ZEBBRA_DOC_SCRIPTS:-zebbra/mkdocs-documentation}" download -A zip -D "$tmp_folder"
 (cd "$tmp_folder" && unzip *.zip)
-\cp -r "$tmp_folder"/**/assets/* make_scripts/mkdocs-documentation/
+\cp -r "$tmp_folder"/**/assets/* .make_scripts/mkdocs-documentation/
+\cp -r "$tmp_folder"/**/assets/.github .make_scripts/mkdocs-documentation/
 rm -rf "$tmp_folder"
 
 
 MAKEFILE=./Makefile
-MAKEFILE_LINE="include ./make_scripts/mkdocs-documentation/mkdocs-documentation-makefile.mk"
+MAKEFILE_LINE="include .make_scripts/mkdocs-documentation/mkdocs-documentation-makefile.mk"
 if [ ! -f $MAKEFILE ]; then
   touch Makefile;
   printf "%s\n" 1 i "$MAKEFILE_LINE" . w | ed -s Makefile
@@ -38,9 +39,9 @@ fi
 
 
 (
-  cd make_scripts/mkdocs-documentation
+  cd .make_scripts/mkdocs-documentation
   find . -type f -iname "*.sh" -exec chmod +x {} \;
 )
 
-poetry install --project make_scripts/mkdocs-documentation/
-poetry run --project make_scripts/mkdocs-documentation/ python make_scripts/mkdocs-documentation/setup_documentation.py
+poetry install --project .make_scripts/mkdocs-documentation/
+poetry run --project .make_scripts/mkdocs-documentation/ python .make_scripts/mkdocs-documentation/setup_documentation.py
--- Makefile
+++ Makefile
@@ -1,9 +1,10 @@
-include ./make_scripts/mkdocs-documentation/mkdocs-documentation-makefile.mk
+include .make_scripts/mkdocs-documentation/mkdocs-documentation-makefile.mk
 
+BRANCH := develop
 update-submodules:
 	echo "Updating submodules"
 	git submodule init
 	git submodule foreach 'git fetch'
-	git submodule foreach 'git checkout develop'
+	git submodule foreach 'git checkout ${BRANCH}'
 	git submodule foreach 'git pull'
 
